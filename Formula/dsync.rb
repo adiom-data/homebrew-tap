005b2a3501b4125b6c750ec3e55ec4f0290a64f6
Alex Komyagin <31981287+alex-thc@users.noreply.github.com>
2026-03-02 20:53:56 -0800

    Dsync 0.24.0
    New version
    
    Closes #2.
    
    Signed-off-by: Alex Komyagin <31981287+alex-thc@users.noreply.github.com>

--- Formula/dsync.rb
+++ Formula/dsync.rb
@@ -1,8 +1,8 @@
 class Dsync < Formula
   desc "Database migration and synchronization tool"
   homepage "https://adiom.io"
-  url "https://github.com/adiom-data/dsync/archive/refs/tags/v0.23.0.tar.gz"
-  sha256 "79103cb24f3936047c43e1b81361fdaf60b8495d21329b9d743c792408596b6c"
+  url "https://github.com/adiom-data/dsync/archive/refs/tags/v0.24.0.tar.gz"
+  sha256 "ced4f5d8b65f2e239a5d671f7c071c52c9e1d10bfd4a8995caf3b36e8448099f"
   license "AGPL-3.0-or-later"
 
   bottle do
